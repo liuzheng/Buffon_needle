7fc712f004d1c0f7e2199a221d0765e79bf727a7
liuzheng712 <liuzheng712@gmail.com>
2012-08-11 13:16:15 +0800

    updata setmap

--- setmap.m
+++ setmap.m
@@ -1,1 +1,14 @@
+clc;cla;
+
 val = get(hpop,'Value');
+D = str2num(get(heditD,'string'));
+d = str2num(get(heditd,'string'));
+L = str2num(get(heditL,'string'));
+n = str2num(get(heditn,'string'));
+N = str2num(get(heditN,'string'));
+h = get(hslider,'value');
+
+for i = 0:n-1
+    line([0 d],[i i]*D,'Color','r','LineWidth',4);
+end
+axis([-0.1*d 1.1*d -0.1*D*n 1.1*D*(n-1)]);
